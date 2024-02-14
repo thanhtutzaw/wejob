3f1347a21b447296051f1d72b2568bf804f98156
thanhtutzaw <zgyi5025@gmail.com>
2024-02-14 22:20:29 +0630

    update : list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,10 @@ function toggleTitle() {
   title.value = toggle.value ? "wonJob" : "Let's Go"
   toggle.value = !toggle.value;
 }
+const jobLists = ref([1, 2, 3])
+function deleteLists(id:string | number) {
+  jobLists.value = jobLists.value.filter(j => j !== id)
+}
 </script>
 
 <template>
@@ -15,10 +19,26 @@ function toggleTitle() {
    <h3>Let's finish your Job search journey together 🙉 🚀</h3>
    <button @click="toggleTitle" type="button">Get Started</button>
     <!-- <TheWelcome /> -->
+   <div class="listsContainer">
+     <button type="button" @click="jobLists.push(jobLists.length + 1)">Add Job Lists</button>
+      <button type="button" @click="jobLists.reverse()">Reverse</button>
+      <button type="button" @click="jobLists = []">Clear</button>
+   </div>
+    <ul v-if="jobLists.length">
+      <li v-bind:key="i" v-for="i of jobLists">
+      {{i}}
+    <button @click="deleteLists(i)">Delete</button>
+    </li>
+    </ul>
   </main>
 </template>
 
 <style>
+.listsContainer{
+  display: flex;
+  gap:1rem;
+
+}
 button{
   /* background-color: rgba(143, 143, 255); */
   background-color: var(--primary-color);
