74e540e68ac4350176d8e18fc33686870d48d124
thanhtutzaw <zgyi5025@gmail.com>
2024-02-18 00:10:07 +0630

    api curd

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
-/* eslint-disable no-undef */
-require('dotenv').config();
+require("dotenv").config();
 var Express = require("express");
-// var MongoClient = require("mongodb").MongoClient;
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
+const { Collection } = require("mongoose");
 var cors = require("cors");
-const multer = require('multer');
-// const multer = require("multer")
+const multer = require("multer");
 var app = Express();
-app.use(cors())
-
-
-
+app.use(cors());
 const CONNECTION_STRING = process.env.MONGO_ATLAS_URL;
-const DATABASE_NAME = "wonjobDB";
-const COLLECTION_NAME = "wonjobCollection";
+const DB_NAME = "wonjobDB";
+const job_posts = "job_posts";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // const client = new MongoClient(CONNECTION_STRING, {
 //     serverApi: {
@@ -22,62 +17,60 @@ const COLLECTION_NAME = "wonjobCollection";
 //         deprecationErrors: true,
 //     }
 // });
-let database;
+let db;
 app.listen(5038, () => {
-    MongoClient.connect(CONNECTION_STRING, (error, client) => {
-        if (error) {
-            console.error('Error connecting to database:', error);
-        } else {
-            database = client.db(DATABASE_NAME);
-            console.log('DB Success');
-
-            app.get('/api/get', (req, res) => {
-                database.collection(COLLECTION_NAME).find({}).toArray((error, result) => {
-                    if (error) {
-                        console.error('Error:', error);
-                        res.status(500).send('Internal Server Error');
-                    }
-                    res.status(200).send(result)
-                })
-
-            })
-
-            // async function run() {
-            //     try {
-            //         // Connect the client to the server	(optional starting in v4.7)
-            //         await client.connect();
-            //         // Send a ping to confirm a successful connection
-            //         await client.db("admin").command({ ping: 1 });
-            //         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-            //     } finally {
-            //         // Ensures that the client will close when you finish/error
-            //         await client.close();
-            //     }
-            // }
-            // run().catch(console.dir);
+  MongoClient.connect(CONNECTION_STRING, (error, client) => {
+    if (error) {
+      console.error("Error connecting to database:", error);
+    } else {
+      db = client.db(DB_NAME);
+      console.log("DB Success");
+      app.get(`/api/${job_posts}`, async (req, res) => {
+        let collection = await db.collection(job_posts);
+        try {
+          let results = await collection.find({}).limit(50).toArray();
+          res.send(results).status(200);
+        } catch (error) {
+          res.status(500).send('Internal Server Error' + error);
         }
-    })
-})
-
-app.post('/api/addJobList', multer().none(), (req, res) => {
-    database.collection(COLLECTION_NAME).count({}, function (numOfDocs) {
+      });
+      app.post(`/api/${job_posts}`, multer().none(), async (req, res) => {
+        let collection = await db.collection(job_posts);
         const newData = {
-            id: String(numOfDocs + 1),
-            title: String(req.query.title) ?? ""
-        }
-        database.collection(COLLECTION_NAME).insertOne(newData);
-        res.send(200).json({ success: 'Added Sucessfully .', data: newData })
-
-    })
-
-})
-app.delete('/api/delete/:id', async (req, res) => {
-    console.log(req)
-    // const id = req.query.id;
-    const query = { _id: ObjectId(req.params.id) };
-    // const id = (req.params.id)
-    // database.collection(COLLECTION_NAME).deleteOne(query)
-    const collection = db.collection(COLLECTION_NAME);
-    let result = await collection.deleteOne(query);
-    res.send("Deleted Successfully : " + result)
-})
+          title: String(req.query.title) ?? "",
+        };
+        let results = await collection.insertOne(newData);
+        res.send("Added List Successfully : " + results).status(200);
+      });
+      app.delete(`/api/${job_posts}`, async (req, res) => {
+        console.log("myreqId " + req.query.id);
+        let collection = await db.collection(job_posts);
+        const query = { _id: ObjectId(req.query.id) };
+        let results = await collection.deleteOne(query);
+        res.send("Deleted Successfully : id - " + req.query.id).status(200);
+      });
+      // app.patch(`/api/${job_posts}/add`, multer().none(), async (req, res) => {
+      //   let collection = await db.collection(job_posts);
+      //   const newData = {
+      //     id: String(await collection.estimatedDocumentCount() + 1),
+      //     title: String(req.query.title) ?? "",
+      //   };
+      //   let results = await collection.insertOne(newData);
+      //   res.send("Edited List Successfully : " + results).status(200);
+      // });
+      // async function run() {
+      //     try {
+      //         // Connect the client to the server	(optional starting in v4.7)
+      //         await client.connect();
+      //         // Send a ping to confirm a successful connection
+      //         await client.db("admin").command({ ping: 1 });
+      //         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      //     } finally {
+      //         // Ensures that the client will close when you finish/error
+      //         await client.close();
+      //     }
+      // }
+      // run().catch(console.dir);
+    }
+  });
+});
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref , type Ref } from 'vue';
 // import TheWelcome from '../components/TheWelcome.vue'
 const title = ref("wonJob")
 const toggle = ref(false)
+const jobTitle = ref("")
 function toggleTitle() {
   title.value = toggle.value ? "wonJob" : "Let's Go"
   toggle.value = !toggle.value;
 }
-const jobLists = ref([1, 2, 3])
-function deleteLists(id:string | number) {
-  jobLists.value = jobLists.value.filter(j => j !== id)
+const jobLists : Ref<{title:string; _id:string}[]> = ref([])
+async function getJobLists() {
+  let newLists = await fetch('http://localhost:5038/api/job_posts').then(res => res.json());
+  console.log(newLists);
+  jobLists.value = newLists;
 }
+async function deleteLists(_id:string | number) {
+  jobLists.value = jobLists.value.filter(j => j._id !== _id)
+  await fetch('http://localhost:5038/api/job_posts?id='+_id, {method:"DELETE"})
+  // await getJobLists();
+}
+async function addLists(e: Event) {
+  // const formData = new FormData();
+  // console.log(formData);
+  console.log(e);
+  console.log(jobTitle);
+  await fetch(`http://localhost:5038/api/job_posts?title=${jobTitle.value}`, {method:"POST" , headers: {
+      "Content-Type": "application/json",
+    },
+  } )
+  await getJobLists();
+  jobTitle.value =''
+}
+onMounted(async() => {
+  await getJobLists();
+})
 </script>
 
 <template>
@@ -20,14 +43,18 @@ function deleteLists(id:string | number) {
    <button @click="toggleTitle" type="button">Get Started</button>
     <!-- <TheWelcome /> -->
    <div class="listsContainer">
-     <button type="button" @click="jobLists.push(jobLists.length + 1)">Add Job Lists</button>
+     <!-- <button type="button" @click="jobLists.push(jobLists.length + 1)">Add Job Lists</button> -->
       <button type="button" @click="jobLists.reverse()">Reverse</button>
       <button type="button" @click="jobLists = []">Clear</button>
    </div>
+   <form @submit.prevent="addLists">
+    <input v-model="jobTitle" class="jobTitleInput" type="search" name="jobTitle" placeholder="Add Job Title" />
+     <button type="submit">Add New</button>
+   </form>
     <ul v-if="jobLists.length">
-      <li v-bind:key="i" v-for="i of jobLists">
-      {{i}}
-    <button @click="deleteLists(i)">Delete</button>
+      <li v-bind:key="i._id" v-for="i of jobLists">
+      {{i.title}}
+    <button @click="deleteLists(i._id)">Delete</button>
     </li>
     </ul>
   </main>
@@ -38,6 +65,16 @@ function deleteLists(id:string | number) {
   display: flex;
   gap:1rem;
 
+}
+.jobTitleInput{
+  padding:.5rem 1rem;
+  border-radius: 1rem;
+  border:1px solid gray;
+  font-size: 1rem;
+&:focus{
+  border: 1px solid var(--primary-color);
+  outline: 1px solid var(--primary-color);
+}
 }
 button{
   /* background-color: rgba(143, 143, 255); */
