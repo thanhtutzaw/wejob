044d6785ffa9f5b81bd0d7f49d9af14b72b8b5a4
thanhtutzaw <zgyi5025@gmail.com>
2024-02-18 19:27:35 +0630

    test : api

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,68 @@
 require("dotenv").config();
-const Express = require("express");
+import Express from "express";
+import MongoClient from "mongodb";
 const app = Express();
-const CONNECTION_STRING = process.env.MONGO_ATLAS_URL;
-const DB_NAME = "wonjobDB"
+const CONNECTION_STRING = process.env.MONGO_ATLAS_URL ?? "";
+const DB_NAME = "wonjobDB";
 const port = process.env.PORT || 5000;
-const job_posts = "job_posts"
+const job_posts = "job_posts";
+// let database;
+const client = new MongoClient(CONNECTION_STRING, {
+  monitorCommands: true,
+});
+const job_posts_collection = client.db(DB_NAME).collection(job_posts);
 app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
+  client.on("commandStarted", (started) => console.log(started));
 
-  MongoClient.connect(CONNECTION_STRING, (error, client) => {
-    if (error) {
-      console.error("Error connecting to database:", error);
-    } else {
-      console.log(process.env.NODE_ENV);
-      database = client.db(DB_NAME);
-      console.log("DB Success  " + "\x1b[0m" + "\x1b[46m" + "➜ " + "\x1b[0m" + " Local:   http://localhost:5038");
+  // console.error("Error connecting to database:", error);
+  console.log(process.env.NODE_ENV);
+  // database = client?.db(DB_NAME);
+  console.log(
+    "DB Success  " +
+    "\x1b[0m" +
+    "\x1b[46m" +
+    "➜ " +
+    "\x1b[0m" +
+    " Local:   http://localhost:5038"
+  );
 
-      app.get('/api', (req, res) => {
-        res.json({
-          api_routes: ["/api/job_posts", "/api/job_posts2"],
-        });
-      });
-      app.get(`/api/${job_posts}`, async (req, res) => {
-        let collection = await database.collection(job_posts);
-        try {
-          let results = await collection.find({}).limit(50).toArray();
-          res.send(results).status(200);
-        } catch (error) {
-          res.status(500).send('Internal Server Error' + error);
-        }
-      });
-      app.post(`/api/${job_posts}`, async (req, res) => {
-        let collection = await database.collection(job_posts);
-        console.log("Request Body:  " + req.body ? JSON.stringify(req.body) : req.body)
-        const body = req.body
-        const newData = {
-          title: String(req.query.title) ?? "",
-          ...body
-        };
-        let results = await collection.insertOne(newData);
-        res.send("Added List Successfully : " + results).status(200);
-      });
-    }
-  })
-})
-app.get('/', (req, res) => {
+  app.post(`/api/${job_posts}`, async (req, res) => {
+    let collection = job_posts_collection;
+    console.log(
+      "Request Body:  " + req.body ? JSON.stringify(req.body) : req.body
+    );
+    const body = req.body;
+    const newData = {
+      title: String(req.query.title) ?? "",
+      ...body,
+    };
+    let results = await collection.insertOne(newData);
+    res.send("Added List Successfully : " + results).status(200);
+  });
+});
+app.get("/", (req, res) => {
   res.json({
     message: `Hello Won Job API listening on ${port} , ${CONNECTION_STRING} .`,
   });
 });
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.json({
     api_routes: ["/api/job_posts", "/api/job_posts2"],
   });
+});
+app.get(`/api/${job_posts}`, async (req, res) => {
+  let collection = job_posts_collection;
+  try {
+    let results = await collection.find({}).limit(50).toArray();
+    res.send(results).status(200);
+  } catch (error) {
+    res.status(500).send("Internal Server Error" + error);
+  }
 });
 // app.get('/api/job_posts', (req, res) => {
 //   res.json({
 //     message: `job_posts from index Hello WonJob Backend API Works ${port}`,
 //   });
 // });
-module.exports = app;
+export default app;
