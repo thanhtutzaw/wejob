a55ce832073d2bba0e61cb0d25c8ba0385ff5edd
thanhtutzaw <zgyi5025@gmail.com>
2024-02-18 13:38:03 +0630

    test : api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const emojis = require('./emojis');
+const job_posts = require('./job_posts');
 
 const router = express.Router();
 
@@ -10,6 +10,6 @@ router.get('/', (req, res) => {
   });
 });
 
-router.use('/emojis', emojis);
+router.use('/job_posts', job_posts);
 
 module.exports = router;
