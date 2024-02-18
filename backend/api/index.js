f36fd163ad4ae4fd0650890e652cd996eef8fd2f
thanhtutzaw <zgyi5025@gmail.com>
2024-02-18 13:35:09 +0630

    test : api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,2 +1,15 @@
-import app from '../app'
-export default app; 
+const express = require('express');
+
+const emojis = require('./emojis');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏',
+  });
+});
+
+router.use('/emojis', emojis);
+
+module.exports = router;
