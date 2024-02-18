a90f755ab0c506089ab1f59547b8a50a8c3d91b3
thanhtutzaw <zgyi5025@gmail.com>
2024-02-18 15:46:22 +0630

    test : api

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 const Express = require("express");
 const app = Express();
 
-// const job_posts = require('./job_posts');
+const job_posts = require('./job_posts');
 
-// const router = Express.Router();
+const router = Express.Router();
 
-// router.get('/', (req, res) => {
-//   res.json({
-//     message: 'API - 👋🌎🌍🌏',
-//   });
-// });
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏',
+  });
+});
 
-// router.use('/job_posts', job_posts);
+router.use('/job_posts', job_posts);
 
-// module.exports = router;
+module.exports = router;
 
 
 
@@ -24,6 +24,6 @@ app.listen(port, () => {
 });
 app.use('/', (req, res) => {
   res.json({
-    message: `Hello WonJob Backend API Works ${port}`,
+    message: `api index Hello WonJob Backend API Works ${port}`,
   });
 });
--- backend/api/job_posts.js
+++ backend/api/job_posts.js
@@ -2,13 +2,9 @@ require("dotenv").config();
 const Express = require("express");
 const app = Express();
 
-// const router = Express.Router();
+const router = Express.Router();
 
-// router.get('/', (req, res) => {
-//     res.json(['😀', '😳', '🙄']);
-// });
 
-// module.exports = router;
 
 
 
@@ -16,8 +12,13 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening: http://localhost:${port}`);
 });
-app.use('/', (req, res) => {
-    res.json({
-        message: `Hello WonJob Backend API Works ${port}`,
-    });
+// app.use('/', (req, res) => {
+//     res.json({
+//         message: `job_posts Hello WonJob Backend API Works ${port}`,
+//     });
+// });
+
+router.get('/', (req, res) => {
+    res.json(['😀', '😳', '🙄']);
 });
+module.exports = router;
