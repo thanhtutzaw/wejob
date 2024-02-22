1aefdc35c87a905f50b1632b1d272fe0b77089f4
thanhtutzaw <zgyi5025@gmail.com>
2024-02-22 20:24:42 +0630

    update : about

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="about">
-    <h1>Developed by <a href="https://thz.vercel.app" target="_blank" rel="noopener" >thanhtutzaw</a></h1>
+    <h1>Won Job</h1>
+    <h2>Job Search Platform built with MEVN Stack</h2>
+    <h3>Developed by <a href="https://thz.vercel.app" target="_blank" rel="noopener" >thanhtutzaw</a></h3>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
+    /* h1{
+      font-size: 1.5rem;
+    } */
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    /* justify-content: center; */
+ text-align: left;
+    /* gap: 1rem; */
   }
+}
+.about{
+  display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+h3 , h2{
+  text-align: center;
 }
 </style>
